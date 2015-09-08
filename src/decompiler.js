1abfe2ea49790f99ca057cec15353a93722bec39
Rogério Chaves <rogeriocfj@gmail.com>
2015-09-08 17:52:31 -0300

    Added basic convertions

--- src/decompiler.js
+++ src/decompiler.js
@@ -1,3 +1,31 @@
-module.exports = function (component) {
-  return '<' + component.type + ' />';
-};
+import React from 'react/addons';
+
+const getProps = component => component._store.originalProps;
+
+const getChildren = component => getProps(component).children;
+
+const getPropsKeys = component =>
+  Object.keys(getProps(component)).filter(prop => prop !== 'children');
+
+const appendStringifiedProp = component => (accumulated, prop) =>
+  `${accumulated} ${prop}="${getProps(component)[prop]}"`;
+
+const stringifyProps = component =>
+  getPropsKeys(component).reduce(appendStringifiedProp(component), '');
+
+const stringifyComposedComponent = component =>
+  `<${component.type}${stringifyProps(component)}>${stringifyItems(getChildren(component))}</${component.type}>`;
+
+const stringifySimpleComponent = component =>
+  `<${component.type}${stringifyProps(component)} />`;
+
+const stringifyComponent = component =>
+  getChildren(component) ? stringifyComposedComponent(component) : stringifySimpleComponent(component);
+
+const stringifyItem = item =>
+  React.addons.TestUtils.isElement(item) ? stringifyComponent(item) : item.toString();
+
+const stringifyItems = components =>
+  [].concat(components).map(stringifyItem).join('')
+
+export default stringifyItems;
--- test/decompiler_spec.js
+++ test/decompiler_spec.js
@@ -1,10 +1,49 @@
-var React = require('react/addons');
-var decompiler = require('decompiler');
+import React from 'react/addons';
+import decompiler from 'decompiler';
 
-describe('decompiler', function() {
-  it('returns the string representation of a simple component', function () {
-    var component = <div />
+describe('decompiler', () => {
+  it('stringify a simple component', () => {
+    let component = <div />;
 
     expect(decompiler(component)).toBe('<div />');
+  });
+
+  it('stringify a simple component with simple props', () => {
+    let component = <div foo="bar" className="baz" />;
+
+    expect(decompiler(component)).toBe('<div foo="bar" className="baz" />');
+  });
+
+  it('stringify a simple component with interpolated props', () => {
+    let component = <div qux={1 + 1} />;
+
+    expect(decompiler(component)).toBe('<div qux="2" />');
+  });
+
+  it('stringify composed components', () => {
+    let component = (<div>
+      <span />
+    </div>);
+
+    expect(decompiler(component)).toBe('<div><span /></div>');
+  });
+
+  it('stringify multiple nested composed components', () => {
+    let component = (<div>
+      <span>
+        <div />
+      </span>
+      <section>
+        <hr />
+      </section>
+    </div>);
+
+    expect(decompiler(component)).toBe('<div><span><div /></span><section><hr /></section></div>');
+  });
+
+  it('stringify components with values inside', () => {
+    let component = <div>Foo</div>;
+
+    expect(decompiler(component)).toBe('<div>Foo</div>');
   });
 });
